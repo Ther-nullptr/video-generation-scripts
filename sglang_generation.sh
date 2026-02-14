9f32e7ab6832650e7b0108e339864ab49834c758
Ther-nullptr <yujin-wa24@mails.tsinghua.edu.cn>
2026-02-14 23:37:45 +0800

    chore: add logic of sampling

--- sglang_generation.sh
+++ sglang_generation.sh
@@ -1,3 +1,7 @@
-for dimension in appearance_style color human_action multiple_objects object_class scene spatial_relationship subject_consistency temporal_flickering temporal_style; do
-    python3 sglang_diffusion_generation.py --dimensions $dimension --save_path output/Wan2.1-T2V-1.3B-Diffusers_$dimension --prompts_dir /home/wyj24/project/VBench/prompts/prompts_per_dimension
+for dimension in subject_consistency human_action object_class
+do
+    for inference_steps in 5 10 15 20 25 30
+    do
+        python3 sglang_diffusion_generation_sampled.py --dimensions $dimension --prompts_dir /home/wyj24/project/VBench/prompts/prompts_per_dimension --num_inference_steps $inference_steps  --save_path /home/wyj24/project/video_generation/output --sample_ratio 0.1 --seed 42
+    done
 done
